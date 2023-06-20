d97884408134d337bafbda48ae960f5d6d2e5b8f
KaedeZzf <zz477@cam.ac.uk>
2023-06-21 06:02:02 +0800

    Normal vectors of a curve can be computed, with direction properly configured. A step of offset, for test purposes only, is computed from the normal vector function, and a plot is generated.

--- blade_geometry/meanline_operation.py
+++ blade_geometry/meanline_operation.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numpy.linalg import norm
 from matplotlib import pyplot as plt
+from shapely import LineString, intersection
 
 # global plot to be used
 fig, ax = plt.subplots()
@@ -29,6 +31,54 @@ def print_curve():
     return
 
 
+# Normalize a vector.
+def normalize(v):
+    return v / norm(v)
+
+
+# Get the normal vector of each point on the curve.
+def get_normal(curve, ref_curve):
+    normals = [np.array([0.0, 0.0])]
+    n = len(curve)
+    for i in range(1, n - 1):
+        next = normalize(curve[i + 1] - curve[i])
+        last = normalize(curve[i] - curve[i - 1])
+        vec = normalize(next - last)
+        vec *= check_direction(curve[i], vec, ref_curve[i])
+        normals.append(vec)
+    normals[0] = normals[1]
+    normals.append(normals[n - 2])
+    return np.array(normals)
+
+
+# Print one of the turbine curves and its steps of offset. For test only.
+def print_step():
+    ax.plot(re_p[:, 0], re_p[:, 1])
+    one_step = re_p + get_normal(re_p, re_s) * 3 * ref_scale(re_p)
+    ax.plot(one_step[:, 0], one_step[:, 1])
+    plt.show()
+    return
+
+
+# Determine the "scale" of the curve; being referred by the offset step length, etc.
+def ref_scale(curve):
+    sum = 0
+    for i in range(1, len(curve)):
+        sum += norm(curve[i] - curve[i - 1])
+    sum /= len(curve)
+    return sum
+
+
+# Check if the vector is pointing "towards" the curve or "away from" the curve:
+# towards: return 1
+# away: return -1
+def check_direction(p, v, ref_p):
+    if np.dot(v, ref_p - p) > 0:
+        return 1
+    return -1
+
+
 re_p = txt2curve("../re_p.txt")
 re_s = txt2curve("../re_s.txt")
-print_curve()
+n_p = get_normal(re_p, re_s)
+print_step()
